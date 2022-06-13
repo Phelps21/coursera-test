75a8e2e8d51d1cba9734f0d02292960f58f6cb89
Wesley Phelps <wesleyphelps@C02H91CSQ05Q.local>
2022-06-13 15:00:51 -0500

    Finished assignment 1 of angularjs

---
+++ as1-solution/app.js
@@ -0,0 +1,26 @@
+(function () {
+'use strict';
+
+angular.module("LunchCheck", [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+
+    $scope.text = ""
+    $scope.funny = ""
+    $scope.check = function () {
+        $scope.items = $scope.text.split(',').length;
+        if ($scope.text == "")
+            $scope.funny = "Please enter data first"
+
+        else if ($scope.items <= 3) 
+            $scope.funny = "Enjoy!"
+
+        else
+            $scope.funny = "TOO MUCH"
+
+    }
+}
+
+})();
