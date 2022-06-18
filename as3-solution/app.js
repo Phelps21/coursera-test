9d51b850c5c3e5c62e229ce8abc0a81afc83c6e6
Wesley Phelps <wesleyphelps@C02H91CSQ05Q.home>
2022-06-17 21:41:59 -0500

    Fixed the remove buttons and added the failed results text

--- as3-solution/app.js
+++ as3-solution/app.js
@@ -10,25 +10,49 @@ function NarrowItDownController(MenuSearchService) {
     var nid = this;
     var mss = MenuSearchService
 
-    nid.search = '';
-    nid.items = [];
+    nid.search;
+    nid.items;
+    nid.empty = false;
 
-    nid.found = function(items) {
-         var promise = mss.getMatchedMenuItems(nid.search)
-         promise.then(function (result) {
-                // process result and only keep items that match
+    // Finds the items from the server and put them in the array
+    nid.found = function() {
+        
+        // If the search is empty then empty page
+        if (!nid.search)
+        {
+            nid.empty = true;
+            nid.items = [];
+            return;
+        }
+
+        var promise = mss.getMatchedMenuItems(nid.search)
+        promise.then(function (results) {            
+
+                // Make a new array so that the results reset after each input
                 var foundItems = [];    
-                for(var item in result.data['menu_items'])
+                var result = results.data['menu_items']
+
+                // In this case item refers to the index of the result array
+                for (var item in result)
                 {
-                    if (result.data['menu_items'][item].name.toLowerCase().includes(nid.search.toLowerCase()))    
-                        foundItems.push(result.data['menu_items'][item]);
+                    if (result[item].description.toLowerCase().includes(nid.search.toLowerCase()))    
+                        foundItems.push(result[item]);
                 }
-                nid.items = foundItems;
-                console.log(nid.items)
+
+                // If no items were found, say so
+                if (foundItems.length == 0)
+                    nid.empty = true;
+
+                else
+                {
+                    nid.empty = false;
+                    nid.items = foundItems;
+                }
     })}
     
+    // Removes an item from the results. Dont mind the index.index
     nid.onRemove = function(index) {
-        nid.items.splice(index, 1)
+        nid.items.splice(index.index, 1)
     }
 }
 
@@ -36,7 +60,7 @@ MenuSearchService.$inject = ['$http']
 function MenuSearchService($http) {
     var mss = this;
 
-    mss.getMatchedMenuItems = function (pattern) {
+    mss.getMatchedMenuItems = function () {
         return $http({
             method: "GET", 
             url: "https://davids-restaurant.herokuapp.com/menu_items.json"
@@ -47,10 +71,6 @@ function MenuSearchService($http) {
 function foundItemsDirective() {
     var ddo = {
         templateUrl: 'foundItems.html',
-        // scope: {
-        //     onRemove: '&',
-        //     foundItems: '<'
-        // },
         controller: NarrowItDownController,
         controllerAs: 'nid',
         bindToController: true,
